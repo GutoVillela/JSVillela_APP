9ce1492a43e877564d43eb66925007167f0571f2
alexandrecoliveira <alecosta19@gmail.com>
2021-05-11 12:52:00 -0300

    Mudança para o parse server no módulo de Redes

---
+++ lib/parse_server/rede_parse.dart
@@ -0,0 +1,93 @@
+import 'package:jsvillela_app/dml/rede_dmo.dart';
+import 'package:jsvillela_app/infra/preferencias.dart';
+import 'package:parse_server_sdk/parse_server_sdk.dart';
+import 'package:jsvillela_app/parse_server/erros_parse.dart';
+
+/// Classe responsável por realizar a conexão com a classe "Rede" do Parse Server.
+class RedeParse{
+
+  //#region Constantes
+
+  /// Constante que define nome da classe "Rede" do Parse Server.
+  static const String NOME_CLASSE = "Rede";
+
+  /// Constante que define o campo "objectId" da classe "Rede" do Parse Server.
+  static const String CAMPO_ID_REDE = "objectId";
+
+  /// Constante que define o campo "nome_rede" da classe "Rede" do Parse Server.
+  static const String CAMPO_NOME_REDE = "nome_rede";
+
+  /// Constante que define o campo "valor unitario" da classe "Rede" do Parse Server.
+  static const String CAMPO_VLR_UNITARIO = "vlr_unitario";
+
+  //#endregion Constantes
+
+  //#region Métodos
+  /// Método responsável por cadastrar uma nova rede no Parse Server.
+  Future<RedeDmo> cadastrarRedeiro(RedeDmo rede) async {
+
+    // Definir informações da rede a ser salva
+    final dadosASalvar = ParseObject(NOME_CLASSE)
+      ..set<String?>(CAMPO_ID_REDE, rede.id)
+      ..set<String?>(CAMPO_NOME_REDE, rede.nome_rede)
+      ..set<double?>(CAMPO_VLR_UNITARIO, rede.valor_unitario_rede);
+    // Gravar dados no Parse Server
+    final response = await dadosASalvar.save();
+
+    if(response.success){
+      // Em caso de sucesso recuperar id do Redeiro cadastrado.
+      rede.id = (response.result as ParseObject).objectId;
+
+      // Retornar redeiro com ID preenchido.
+      return rede;
+    }
+    else{
+      if(response.error != null)
+        return Future.error(ErrosParse.obterDescricao(response.error!.code));
+      else
+        return Future.error("Aconteceu um erro inesperado!");
+    }
+  }
+
+  /// Método responsável por obter a lista de Redes do Parse Server de forma paginada.
+  Future<List<RedeDmo>> obterListaDeRedePaginadas(int registrosAPular, String? filtroPorNome) async{
+
+    // Criando consulta
+    final queryBuilder = QueryBuilder(ParseObject(NOME_CLASSE))
+      ..setAmountToSkip(registrosAPular)
+      ..setLimit(Preferencias.QUANTIDADE_REGISTROS_LAZY_LOADING)
+      ..orderByAscending(CAMPO_NOME_REDE);
+
+    // Definir filtro, caso tenha sido fornecido.
+    if(filtroPorNome != null)
+      queryBuilder.whereStartsWith(CAMPO_NOME_REDE, filtroPorNome);
+
+    // Executar consulta
+    final response = await queryBuilder.query();
+
+    if(response.success){
+
+      // Em caso de sucesso retornar objeto de redeiros preenchido.
+      if(response.results != null){
+        //Montar lista com objetos de retorno
+        List<RedeDmo> lista = [];
+
+        // Converter lista retornada para lista de RedeDmo
+        lista.addAll(response.results!.map((e) => RedeDmo.fromParse(e)));
+
+        return lista;
+      }
+      else
+        return [];
+    }
+    else{
+      if(response.error != null)
+        return Future.error(ErrosParse.obterDescricao(response.error!.code));
+      else
+        return Future.error("Aconteceu um erro inesperado!");
+    }
+  }
+
+//#endregion Métodos
+
+}
---
+++ lib/stores/consultar_rede_store.dart
@@ -0,0 +1,95 @@
+import 'package:jsvillela_app/dml/rede_dmo.dart';
+import 'package:jsvillela_app/infra/preferencias.dart';
+import 'package:jsvillela_app/parse_server/rede_parse.dart';
+import 'package:mobx/mobx.dart';
+
+part 'consultar_rede_store.g.dart';
+
+class ConsultarRedeStore = _ConsultarRedeStore with _$ConsultarRedeStore;
+
+abstract class _ConsultarRedeStore with Store{
+
+  //#region Construtor(es)
+
+  //#endregion Construtor(es)
+
+  //#region Observables
+  ///Atributo observável que define se classe está processando uma tarefa.
+  @observable
+  bool processando = false;
+
+  /// Atributo observável que define o termo de busca definido em tela.
+  @observable
+  String termoDeBusca = "";
+
+  ///Atributo observável que define a lista de redeiros a ser exibida em tela.
+  ObservableList<RedeDmo> listaDeRede = ObservableList<RedeDmo>();
+
+  ///Atributo observável que define se existem mais registros a serem buscados.
+  @observable
+  bool temMaisRegistros = true;
+
+  /// Atributo observável que define se ocorreu algum erro durante processo de login.
+  @observable
+  String? erro;
+  //#endregion Observables
+
+  //#region Computed
+
+  //#endregion Computed
+
+  //#region Actions
+
+  ///Action que define o valor do atributo observável "senha".
+  @action
+  void setTermoDeBusca(String value) => termoDeBusca = value;
+
+  ///Action que define o valor do atributo observável "listaDeRede".
+  @action
+  void setListaDeRedeiros(List<RedeDmo> value) {
+    listaDeRede.clear();
+    listaDeRede.addAll(value);
+  }
+
+  /// Action responsável por obter a lista de Rede do Parse Server de forma paginada.
+  @action
+  Future<void> obterListaDeRedePaginada(bool limpaLista, String? filtroPorNome) async{
+
+    // Indicar que classe iniciou o processamento.
+    if(limpaLista){
+      processando = true;
+      listaDeRede.clear();
+    }
+
+    try{
+
+      // Consultar lista no Parse Server
+      List<RedeDmo> listaObtida = await RedeParse().obterListaDeRedePaginadas(listaDeRede.length, filtroPorNome);
+
+      // Definir se existem mais registros a serem buscados futuramente.
+      temMaisRegistros = listaObtida.length >= Preferencias.QUANTIDADE_REGISTROS_LAZY_LOADING;
+
+      // Adicionar itens da consulta paginada na lista de itens da tela
+      listaDeRede.addAll(listaObtida);
+
+      // Indicar que classe finalizou o processamento.
+      if(limpaLista)
+        processando = false;
+    }
+    catch (e){
+      erro = e.toString();
+
+      // Indicar que classe finalizou o processamento.
+      if(limpaLista)
+        processando = false;
+    }
+  }
+
+  /// Action responsável por obter a lista de Rede do Parse Server de forma paginada aplicando filtro de nome.
+  @action
+  Future<void> obterListaDeRedePaginadaComFiltro(String filtroNome) async{
+    await obterListaDeRedePaginada(true, filtroNome);
+  }
+
+  //#endregion Actions
+}
